aa8c5294a2e8dceaa53bdf3ef7db1a6d82df669d
João Portela <joao.portela@gmail.com>
2011-12-07 18:17:21 +0000

    Initial commit, with c2dm src from google

---
+++ src/pt/up/fe/cmov/propertymarket/PropertyMarketActivity.java
@@ -0,0 +1,13 @@
+package pt.up.fe.cmov.propertymarket;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PropertyMarketActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
