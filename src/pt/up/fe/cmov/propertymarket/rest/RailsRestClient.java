68468c42025b467b493e9b4f31c24d7909abb678
João Portela <joao.portela@gmail.com>
2011-12-09 23:17:01 +0000

    changed server url

--- src/pt/up/fe/cmov/propertymarket/rest/RailsRestClient.java
+++ src/pt/up/fe/cmov/propertymarket/rest/RailsRestClient.java
@@ -6,7 +6,7 @@ import org.json.JSONObject;
 
 public class RailsRestClient {
 	
-	public final static String SERVER_URL = "http://192.168.1.5:3000";
+	public final static String SERVER_URL = "http://10.0.0.144:3000";
 
 
 
